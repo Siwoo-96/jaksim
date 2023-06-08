c56c958c8756672c223dc06c21f6580b0d3c3a8f
Dong Hyeon Oh <dev0doraemon@gmail.com>
2023-06-08 11:07:02 +0900

    UPDATES (VERSION 1.0 -> 1.1) - 라이브러리 경로 수정(경로가 잘못 잡혀있어 참조 불가였음) - 영화 상세페이지 댓글 기능 추가

--- js/subApp.js
+++ js/subApp.js
@@ -1,31 +1,27 @@
 import { detailView } from "./detailView.js";
+import { PseudoDB } from "./handleComment/PseudoDB.js";
+import { handleSubmit } from "./handleComment/commentHandler.js";
+import { handleCommentEdit } from "./handleComment/commentHandleEdit.js";
+
+// export
+export const db = new PseudoDB();
+localStorage.clear();
 
 document.addEventListener("DOMContentLoaded", () => {
     detailView();
+    const commentForm = document.querySelector("#comment-form");
+    commentForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        handleSubmit();
+
+        document.querySelector("#content").value = null;
+        document.querySelector("#user-id").value = null;
+        document.querySelector("#user-password").value = null;
+    });
+
+    const commentList = document.querySelector("#comment-list");
+    commentList.addEventListener("click", handleCommentEdit);
     const comment = document.querySelector("#comment-list");
 });
 
-
-
-// import { PseudoDB } from "./PseudoDB.js";
-// import { handleSubmit } from "./commentHandler.js";
-// import { handleCommentEdit } from "./commentHandleEdit.js";
-
-// // export
-// export const db = new PseudoDB();
-// localStorage.clear();
-
-
-// // start
-// const commentForm = document.querySelector("#comment-form");
-// commentForm.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     handleSubmit();
-
-//     document.querySelector("#content").value = null;
-//     document.querySelector("#user-id").value = null;
-//     document.querySelector("#user-password").value = null;
-// });
-
-// const commentList = document.querySelector("#comment-list");
-// commentList.addEventListener("click", handleCommentEdit);
+// start
