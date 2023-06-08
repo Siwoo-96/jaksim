0c2970c001e3cfd981fc713c80a3ce8e8c4bad17
KimJaeHyeok <master_catch@naver.com>
2023-06-08 10:25:55 +0900

    v1.1 (JH)

--- js/app.js
+++ js/app.js
@@ -7,83 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const dropDown = document.querySelector(".dropDown");
     dropDown.addEventListener("click", chooseRegion);
 
-<<<<<<< HEAD
-async function movieData() {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDFjNjgxZjk3MjYyMjY4NWFkYjJkZWQ3MGVlNWU2ZiIsInN1YiI6IjY0NzZkZjNjMTJjNjA0MDBlMWRjNmNjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jqyMjkuWeOJzkj7UauO06VhfP3Ohg7aD0LigJgVHgyw",
-    },
-  };
-  let response = await fetch(
-    `${URL}/movie/popular?api_key=${KEY}&language=${BASE_LANG}&region=${BASE_REGION}`,
-    options
-  );
-  let data = await response.json();
-  let movies = data["results"];
-
-  return movies;
-}
-
-const nation = document.querySelector("#nation");
-const dropDown = document.querySelector(".dropDown");
-
-dropDown.addEventListener('click', (event) => {
-  let target = event.target;
-
-  if (target.textContent === '한국') {
-    BASE_LANG = 'ko';
-    BASE_REGION = 'KR';
-    nation.textContent = '한국';
-  } else if (target.textContent === '미국') {
-    BASE_LANG = 'en';
-    BASE_REGION = 'US';
-    nation.textContent = '미국';
-  } else if (target.textContent === '일본') {
-    BASE_LANG = 'ja';
-    BASE_REGION = 'JP';
-    nation.textContent = '일본';
-  } else if (target.textContent === '중국') {
-    BASE_LANG = 'zh';
-    BASE_REGION = 'CN';
-    nation.textContent = '중국';
-  }
-
-  cardView();
-})
-
-
-// search
-function movieSearch(clk) {
-  const movieCards = document.querySelectorAll(".movieCard");
-
-  movieCards.forEach((card) => {
-    const title = card.childNodes[2]['nextSibling']['innerText'].split(' ').join('').toLowerCase();
-
-    for (let i = 0; i < title.length; i++) {
-      if (title.includes(`${clk}`)) {
-        card.style.display = "grid";
-      } else {
-        card.style.display = "none";
-      }
-    }
-  });
-}
-
-document.addEventListener('keyup', function (event) {
-  let clk = document.getElementById("searchInput").value.toLowerCase();
-
-  movieSearch(clk);
-})
-
-export { movieData };
-=======
     document.addEventListener("keyup", function (event) {
-        let mes = document.getElementById("searchInput").value.toLowerCase();
+        let clk = document.getElementById("searchInput").value.toLowerCase();
     
-        searchMovie(mes);
+        searchMovie(clk);
     });
 });
->>>>>>> 0054cb14b042529ae8ee026df0b6a52cb47ce782
--- js/detailView.js
+++ js/detailView.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { movieData } from "./movieApi.js";
-=======
 import { fetchMovieData } from "./fetchMovieData.js";
->>>>>>> 0054cb14b042529ae8ee026df0b6a52cb47ce782
 
 async function detailView() {
     const urlParams = new URL(location.href).searchParams;
@@ -10,21 +6,6 @@ async function detailView() {
     const lang = urlParams.get("lang");
     let movies = await fetchMovieData(lang);
 
-<<<<<<< HEAD
-  const result = movies.find((x) => x.id === id);
-  if (!result) {
-    alert("페이지의 정보값이 올바르지 않습니다");
-    history.back();
-  }
-  let tempHTML = `<div class="movieCard" data-id="${result.id}">
-  <img src="https://image.tmdb.org/t/p/w500${result.poster_path}">
-  <h3>${result.title}</h3>
-  <p>${result.overview}</p>
-  <p class="rate">Rating: ${result.vote_average}</p>
-  </div>`;
-  const detailMovie = document.querySelector(".subBox");
-  detailMovie.insertAdjacentHTML("beforeend", tempHTML);
-=======
     // 예외처리
     const result = movies.find((x) => x.id === id);
     if (!result) {
@@ -41,7 +22,6 @@ async function detailView() {
             <p class="rate">Rating: ${result.vote_average}</p>
         </div>
     `;
->>>>>>> 0054cb14b042529ae8ee026df0b6a52cb47ce782
 }
 
 export { detailView };
